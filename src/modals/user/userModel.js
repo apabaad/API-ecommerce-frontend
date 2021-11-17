3efc0b20623a574a29ab98e2c2421cf594b46a0b
apabaad <apabaad@yahoo.com>
2021-11-17 15:08:06 +1100

    find user by email api created

--- src/modals/user/userModel.js
+++ src/modals/user/userModel.js
@@ -8,3 +8,7 @@ export const createUser = (newUser) => {
     throw new Error(error);
   }
 };
+
+export const findUserByEmail = (email) => {
+  return userSchema.findOne({ email }); //{email} because mongodb always takes parameter as an object. just email wont work.
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { createUser } from '../modals/user/userModel.js';
+import { createUser, findUserByEmail } from '../modals/user/userModel.js';
 import { newFormValidation } from '../middlewares/validation.middleware.js';
 const Router = express.Router();
 
 Router.post('/', newFormValidation, async (req, res) => {
   try {
-    console.log(req.body);
     const result = await createUser(req.body);
 
     if (result) {
@@ -28,4 +27,27 @@ Router.post('/', newFormValidation, async (req, res) => {
   }
 });
 
+Router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const result = await findUserByEmail(email);
+
+    if (result) {
+      return res.json({
+        status: 'success',
+        message: 'login successful',
+        result,
+      });
+    }
+    return res.json({
+      status: 'error',
+      message: 'Authentication Failed',
+    });
+  } catch (error) {
+    res.json({
+      status: 'error',
+      message: 'Failed to login',
+    });
+  }
+});
 export default Router;
